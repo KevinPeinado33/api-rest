ef79d15b903381576e21cafcb486e0454d0992b2
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2021-05-01 20:26:57 -0500

    resolviendo problema de versions

--- src/main/java/com/attomsoft/api/utils/entity/Role.java
+++ src/main/java/com/attomsoft/api/utils/entity/Role.java
@@ -5,39 +5,39 @@ import java.io.Serializable;
 import java.util.List;
 
 @Entity
-@Table(name = "roles")
+@Table( name = "roles" )
 public class Role implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
     private Long id;
 
-    @Column(unique = true, length = 20)
+    @Column( unique = true, length = 20 )
     private String name;
 
     /**
      * Usamos el cascade = CascadeType.ALL, para eliminar los que esten relacionados
      */
-    @ManyToMany(mappedBy = "roles")
-    private List<Usuario> users;
+    @ManyToMany( mappedBy = "roles" )
+    private List< Usuario > users;
 
-    public List<Usuario> getUsers() {
+    public List< Usuario > getUsers( ) {
         return users;
     }
 
-    public void setUsers(List<Usuario> users) {
+    public void setUsers( List< Usuario > users ) {
         this.users = users;
     }
 
-    public Long getId() {
+    public Long getId( ) {
         return id;
     }
 
-    public String getName() {
+    public String getName( ) {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName( String name ) {
         this.name = name;
     }
 
